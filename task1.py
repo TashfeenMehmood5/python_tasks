d6d0c99c2df494a32d130908c1561fe60fa5f8ea
TashfeenMehmood5 <tashfeenmehmood510@gmail.com>
2023-09-12 15:47:45 +0500

    message

---
+++ task1.py
@@ -0,0 +1,27 @@
+# area of circle
+pi = 3.14
+r = 3
+area = pi*r*r
+print("area of circle",area)
+
+#area of rectangle
+width= 5
+lenght = 8
+area = width*lenght
+print("area of circle",area)
+
+#arae of triangle
+base = 8
+width= 9
+lenght = 10
+area = 0.5*(base*width*lenght)
+print("area of circle",area)
+#convert farenheit to celcius
+f = 45 
+c = ((f-32)*5)/9
+print("temperature farenheit into celcius ", c)
+
+#convert celcius to farenheit
+c = 45 
+f = c*1.8+32
+print("temperature celcius into farenheit ", f)
